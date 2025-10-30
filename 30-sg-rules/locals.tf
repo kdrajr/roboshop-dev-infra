7e4da871f183a0093a9496f537d1889e41631c75
Dheeraj <31932986+kdrajr@users.noreply.github.com>
2025-10-30 20:12:15 +0530

    terraform

--- 30-sg-rules/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  vpc_id             = data.aws_ssm_parameter.vpc_id.value
-  bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
-  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
-  redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
-  mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-  rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
-  backend-alb_sg_id = data.aws_ssm_parameter.backend-alb_sg_id.value
-}
