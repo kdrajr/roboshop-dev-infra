222ba4469a04c5e64fbd42fdd2324275f0361fca
Dheeraj <31932986+kdrajr@users.noreply.github.com>
2025-11-06 19:36:25 +0530

    changed to ansible-playbook command for shell scripts

--- 90-components/component.sh
+++ 90-components/component.sh
@@ -3,28 +3,28 @@
 component=$1
 environment=$2
 
-# REPO_URL=https://github.com/kdrajr/ansible-roboshop-roles-tf.git
-# ANSIBLE_DIR=/opt/roboshop/ansible
-# REPO_DIR=ansible-roboshop-roles-tf
-# mkdir -p $ANSIBLE_DIR
+REPO_URL=https://github.com/kdrajr/ansible-roboshop-roles-tf.git
+ANSIBLE_DIR=/opt/roboshop/ansible
+REPO_DIR=ansible-roboshop-roles-tf
+mkdir -p $ANSIBLE_DIR
 
 
 dnf install ansible -y
 mkdir -p /var/log/roboshop
 touch /var/log/roboshop/ansible.log
 
-# cd $ANSIBLE_DIR
+cd $ANSIBLE_DIR
 
-# if [ -d $REPO_DIR ]; then
-#     cd $REPO_DIR
-#     git pull
-# else
-#     git clone $REPO_URL
-#     cd $REPO_DIR
-# fi
+if [ -d $REPO_DIR ]; then
+    cd $REPO_DIR
+    git pull
+else
+    git clone $REPO_URL
+    cd $REPO_DIR
+fi
 
-# ansible-playbook -i inventory.ini -e component=$component -e env=$environment main.yaml
+ansible-playbook -i inventory.ini -e component=$component -e env=$environment main.yaml
 
-ansible-pull -U https://github.com/kdrajr/ansible-roboshop-roles-tf.git -e component=$component -e env=$environment main.yaml
+#ansible-pull -U https://github.com/kdrajr/ansible-roboshop-roles-tf.git -e component=$component -e env=$environment main.yaml
 
 
